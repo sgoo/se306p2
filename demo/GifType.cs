d8b16ee96efa380f62f96363e6804e20045fd5b0
Scott Goodhew <scott.goodhew0@gmail.com>
2012-09-23 20:57:02 +1200

    added more gifs

--- demo/GifType.cs
+++ demo/GifType.cs
@@ -1,22 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Collections.ObjectModel;
 
 namespace demo {
-    public class GifType : DataTemplate {
-        private string name;
-        private string[] gifs;
-        public string Name {
-            get { return name; }
-        }
-        public string[] GIFs {
-            get { return gifs; }
-        }
-        public GifType(string name, string[] gifs) {
-            this.name = name;
-            this.gifs = gifs;
-        }
-    }
+	public class GifType : DataTemplate {
+		private String name;
+		private ObservableCollection<String> gifs;
+		private String desc;
+		public String Name {
+			get { return name; }
+		}
+		public ObservableCollection<String> GIFs {
+			get { return gifs; }
+		}
+		public ObservableCollection<String> MainGifs {
+			get {
+				ObservableCollection<String> output = new ObservableCollection<String>(gifs);
+				output.RemoveAt(0);
+				return output;
+			}
+		}
+
+		public String Description {
+			get {
+				return desc;
+			}
+		}
+		public GifType(string name, string desc) {
+			this.name = name;
+			this.desc = desc;
+			this.gifs = new ObservableCollection<String>();
+		}
+
+	}
 }
