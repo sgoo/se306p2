e740d40eeffee9ebe9f03ff69870611d7a3c9887
Scott Goodhew <scott.goodhew0@gmail.com>
2012-09-26 13:32:24 +1200

    renamed GifControl to WPFControls

--- GifControl/Properties/Resources.Designer.cs
+++ GifControl/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GifControl.Properties {
+namespace WPFControls.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace GifControl.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GifControl.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WPFControls.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- GifControl/Properties/Settings.Designer.cs
+++ GifControl/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GifControl.Properties {
+namespace WPFControls.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
