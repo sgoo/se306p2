60d6bd0a6a80052a3fc0f8ca9760da2b260dd95c
Scott Goodhew <scott.goodhew0@gmail.com>
2012-09-30 16:45:21 +1300

    add sample video

--- se306p2/Pages/BaseInfo.xaml.cs
+++ se306p2/Pages/BaseInfo.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,79 +14,71 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
-namespace se306p2
-{
-    /// <summary>
-    /// Interaction logic for InfoOnEEE.xaml
-    /// </summary>
-    /// 
-    
-    public partial class BaseInfo : UserControl
-    {
-        DispatcherTimer timer;
-        int ctr = 0;
-        public BaseInfo()
-        {
-            InitializeComponent();
-            timer = new DispatcherTimer();
-            timer.Interval = new TimeSpan(0, 0, 2);
-            timer.Tick += new EventHandler(timer_Tick);
+namespace se306p2 {
+	/// <summary>
+	/// Interaction logic for InfoOnEEE.xaml
+	/// </summary>
+	/// 
 
-        }
+	public partial class BaseInfo : UserControl {
+		DispatcherTimer timer;
+		int ctr = 0;
+		public BaseInfo() {
+			InitializeComponent();
+			
+			// load video
+			Util.LoadVideo("NandaanParindey.mp4",_mediaElement);
 
-        private void _mediaElement_MediaEnded(object sender, RoutedEventArgs e)
-        {
-            _mediaElement.Stop();
-        }
-        private void _mediaElement_PreviewTouchDown(object sender, InputEventArgs e)
-        {
-            if (_mediaElement.Tag.ToString() == "pause")
-            {
-                _mediaElement.Tag = "play";
-                _mediaElement.Play();
+			timer = new DispatcherTimer();
+			timer.Interval = new TimeSpan(0, 0, 2);
+			timer.Tick += new EventHandler(timer_Tick);
 
-            }
-            else
-            {
-                _mediaElement.Tag = "pause";
-                _mediaElement.Pause();
+		}
 
-            }
-        }
+		private void _mediaElement_MediaEnded(object sender, RoutedEventArgs e) {
+			_mediaElement.Position = TimeSpan.Zero;
+			_mediaElement.Play();
+		}
+		private void _mediaElement_PreviewTouchDown(object sender, InputEventArgs e) {
+			if (_mediaElement.Tag.ToString() == "pause") {
+				_mediaElement.Tag = "play";
+				_mediaElement.Play();
 
-        protected override void OnInitialized(EventArgs e)
-        {
-            base.OnInitialized(e);
-            DataContext = this;
-            _mediaElement.Play();
-        }
+			} else {
+				_mediaElement.Tag = "pause";
+				_mediaElement.Pause();
 
-        void timer_Tick(object sender, EventArgs e)
-        {
-            ctr++;
-            if (ctr > 5)
-            {
-                ctr = 1;
-            }
-            PlaySlideShow(ctr);
-        }
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            ctr = 1;
-            PlaySlideShow(ctr);
-        }
-        private void PlaySlideShow(int ctr)
-        {
-            BitmapImage image = new BitmapImage();
-            image.BeginInit();
-            string filename = "Resources/Ece" + ctr + ".jpg";
-            image.UriSource = new Uri(filename, UriKind.Relative);
-            image.EndInit();
-            myImage.Source = image;
-            myImage.Stretch = Stretch.Uniform;
-            timer.IsEnabled = true;
-        }
+			}
+		}
+
+		protected override void OnInitialized(EventArgs e) {
+			base.OnInitialized(e);
+			DataContext = this;
+			_mediaElement.Play();
+		}
+
+		void timer_Tick(object sender, EventArgs e) {
+			ctr++;
+			if (ctr > 5) {
+				ctr = 1;
+			}
+			PlaySlideShow(ctr);
+		}
+		private void Window_Loaded(object sender, RoutedEventArgs e) {
+			ctr = 1;
+			PlaySlideShow(ctr);
+		}
+		private void PlaySlideShow(int ctr) {
+			BitmapImage image = new BitmapImage();
+			image.BeginInit();
+			string filename = "Resources/Ece" + ctr + ".jpg";
+			image.UriSource = new Uri(filename, UriKind.Relative);
+			image.EndInit();
+			myImage.Source = image;
+			myImage.Stretch = Stretch.Uniform;
+			timer.IsEnabled = true;
+		}
 
 
-    }
+	}
 }
---
+++ se306p2/Util.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.IO;
+
+namespace se306p2 {
+	class Util {
+		public static void LoadVideo(String videoName, MediaElement media) {
+			DateTime start = DateTime.Now;
+			String[] drives = Environment.GetLogicalDrives();
+			foreach (String drive in drives) {
+				if (File.Exists(drive + videoName)) {
+					media.Source = new Uri(drive + videoName);
+					break;
+				}
+			}
+			Console.WriteLine("Took {0} to load {1}", (DateTime.Now - start).TotalMilliseconds, videoName);
+		}
+	}
+}
