7a0c1cfdad68ec7c4f5f69e25ea4a91e4f5859a3
charzpiscean99 <charzpiscean99@gmail.com>
2012-10-04 00:26:08 +1300

    Pauline + Charu - Added title and surface library control to info page template

--- se306p2/Pages/BaseInfo.xaml.cs
+++ se306p2/Pages/BaseInfo.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace se306p2 {
 	/// <summary>
@@ -20,132 +22,79 @@ namespace se306p2 {
 	/// 
 
 	public partial class BaseInfo : UserControl {
-		int ctr = 0;
-		public bool label1 = false;
-		public bool label2 = false;
-		public bool label3 = false;
-		public bool label4 = false;
-		public bool label5 = false;
-		public bool label6 = false; DispatcherTimer timer;
 
 		public BaseInfo() {
 			InitializeComponent();
-
-			timer = new DispatcherTimer();
-			timer.Interval = new TimeSpan(0, 0, 2);
-			timer.Tick += new EventHandler(timer_Tick);
-			// load video
-			Util.LoadVideo("NandaanParindey.mp4", _mediaElement);
-
 		}
 
-		private void _mediaElement_MediaEnded(object sender, RoutedEventArgs e) {
-			_mediaElement.Position = TimeSpan.Zero;
-			_mediaElement.Play();
-		}
-		private void _mediaElement_PreviewTouchDown(object sender, InputEventArgs e) {
-			if (_mediaElement.Tag.ToString() == "pause") {
-				_mediaElement.Tag = "play";
-				_mediaElement.Play();
+        #region OnInitialized
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
 
-			} else {
-				_mediaElement.Tag = "pause";
-				_mediaElement.Pause();
-			}
-		}
+            // Create an ObservableCollection, and add the items.
+            // LibraryBar.ItemsSource should implement INotifyCollectionChanged
+            // in order for the built-in drag-and-drop capability to work properly.
+            ObservableCollection<DataThumbnail> items = new ObservableCollection<DataThumbnail>();
 
-		protected override void OnInitialized(EventArgs e) {
-			base.OnInitialized(e);
-			DataContext = this;
-			_mediaElement.Play();
-		}
+            items.Add(new DataThumbnail(@"..\Resources\blue0.jpg"));
+            items.Add(new DataThumbnail(@"..\Resources\blue1.jpg"));
+            items.Add(new DataThumbnail(@"..\Resources\blue2.jpg"));
+            items.Add(new DataThumbnail(@"..\Resources\blue4.jpg"));
+            items.Add(new DataThumbnail(@"..\Resources\green0.jpg"));
+            items.Add(new DataThumbnail(@"..\Resources\green1.jpg"));
+            items.Add(new DataThumbnail(@"..\Resources\green2.jpg"));
+            MainLibraryBar.ItemsSource = items;
+            // Get the default view and establish grouping.
+            ICollectionView defaultView = CollectionViewSource.GetDefaultView(items);
+            defaultView.GroupDescriptions.Add(new PropertyGroupDescription("GroupName"));
+        }
+        #endregion
+		
 
-		private void PlaySlideShow(int ctr) {
-			BitmapImage image = new BitmapImage();
-			image.BeginInit();
-			string filename = "/Resources/Ece" + ctr + ".jpg";
-			image.UriSource = new Uri(filename, UriKind.Relative);
-			image.EndInit();
-			myImage.Source = image;
-			myImage.Stretch = Stretch.Uniform;
-			label1 = false;
-			label2 = false;
-			label3 = false;
-			label4 = false;
-			label5 = false;
-			label6 = false;
-			timer.IsEnabled = true;
-
-
-		}
-
-		private void Window_Loaded(object sender, RoutedEventArgs e) {
-			ctr = 4;
-			PlaySlideShow(ctr);
-		}
-
-		private void Label_MouseMove(object sender, InputEventArgs e) {
-			label1 = true;
-			label4 = true;
-			checkFlickLeft();
-
-		}
-
-		private void Label2_MouseMove(object sender, InputEventArgs e) {
-			label2 = true;
-			label5 = true;
-		}
-
-		private void Label3_MouseMove(object sender, InputEventArgs e) {
-			label3 = true;
-			label6 = true;
-			checkFlickRight();
-		}
-
-		private void checkFlickRight() {
-
-			if (label1 && label2 && label3) {
-				if (ctr >= 5 || ctr < 0) {
-					ctr = 0;
-				} else {
-					ctr++;
-				}
-
-				PlaySlideShow(ctr);
-
-			}
-			label1 = false;
-			label2 = false;
-			label3 = false;
-
-
-		}
-
-		private void checkFlickLeft() {
-
-			if (label4 && label5 && label6) {
-				if (ctr > 5 || ctr <= 0) {
-					ctr = 5;
-				} else {
-					ctr--;
-				}
-
-				PlaySlideShow(ctr);
-
-			}
-
-			label4 = false;
-			label5 = false;
-			label6 = false;
-
-		}
-		void timer_Tick(object sender, EventArgs e) {
-			ctr++;
-			if (ctr > 5) {
-				ctr = 1;
-			}
-			PlaySlideShow(ctr);
-		}
 	}
+
+    #region DataThumbnailClass
+    /// <summary>
+    /// Represents an image thumbnail with an associated label and a group name.
+    /// </summary>
+    public class DataThumbnail
+    {
+       
+        private string fileName;
+        private BitmapImage bitmap;
+
+        /// <summary>
+        /// Creates a new instance of the DataThumbnail class.
+        /// </summary>
+        /// <param name="fileName">The file name of the image.</param>
+        /// <param name="label">The text associated with the image.</param>
+        /// <param name="groupName">The group name for the image.</param>
+        public DataThumbnail(string fileName)
+        {
+            // For simplicity, not checking for null parameters.
+            this.fileName = fileName;
+            this.bitmap = new BitmapImage(new Uri(fileName, UriKind.Relative));
+        }
+
+        /// <summary>
+        /// Gets the label of this instance.
+        /// </summary>
+       
+
+        /// <summary>
+        /// Gets the group name of this instance.
+        /// </summary>
+       
+
+        /// <summary>
+        /// Gets the image used by this instance.
+        /// </summary>
+        public BitmapSource Bitmap
+        {
+            get { return bitmap; }
+        }
+    }
+    #endregion
 
 }
