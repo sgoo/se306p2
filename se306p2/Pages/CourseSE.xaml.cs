9e77452f4fbba27bd76334507ddc54cb1b52e987
unknown <rlin050@en-301-0167-024.engad.foe.auckland.ac.nz>
2012-10-05 17:18:31 +1300

    finished swipe on se courses page

--- se306p2/Pages/CourseSE.xaml.cs
+++ se306p2/Pages/CourseSE.xaml.cs
@@ -16,7 +16,7 @@ using Microsoft.Surface.Presentation;
 using Microsoft.Surface.Presentation.Controls;
 using Microsoft.Surface.Presentation.Input;
 using System.Collections.ObjectModel;
-
+using System.Windows.Media.Animation;
 
 namespace se306p2
 {
@@ -25,6 +25,10 @@ namespace se306p2
     /// </summary>
     public partial class CourseSE : UserControl
     {
+        private ObservableCollection<Grid> pages = new ObservableCollection<Grid>();
+        private ObservableCollection<Grid> subs = new ObservableCollection<Grid>();
+        private int indicator = 0;
+
         public CourseSE()
         {
             InitializeComponent();
@@ -40,12 +44,16 @@ namespace se306p2
         {
             base.OnInitialized(e);
             DataContext = this;
+            
             pages.Add(_page1);
             pages.Add(_page2);
             pages.Add(_page3);
+
+            subs.Add(_sub1);
+            subs.Add(_sub2);
+            subs.Add(_sub3);
         }
-        private ObservableCollection<Grid> pages = new ObservableCollection<Grid>();
-        private int indicator = 0;
+
         private void PreviousButton(object sender, RoutedEventArgs e)
         {
             SwitchPrevious();
@@ -59,10 +67,34 @@ namespace se306p2
             if (IsPreviousValid())
             {
                 Grid currentpage = pages.ElementAt(indicator);
+                Grid currentSub = subs.ElementAt(indicator);
                 indicator -= 1;
+                Grid nextSub = subs.ElementAt(indicator);
                 Grid nextpage = pages.ElementAt(indicator);
-                currentpage.Visibility = System.Windows.Visibility.Collapsed;
+
+
+
+                // animations
+                Storyboard sb1 = (Storyboard)Resources["SlideOriginToLeft"];
+                Storyboard sb2 = (Storyboard)Resources["SlideRightToOrigin"];
+                sb1.Begin(currentSub);
+                sb2.Begin(nextSub);
+
+                Storyboard sb3 = (Storyboard)Resources["FadeOut"];
+                Storyboard sb4 = (Storyboard)Resources["FadeIn"];
+
+                sb4.SeekAlignedToLastTick(new TimeSpan(0, 0, 10));
+                sb3.SeekAlignedToLastTick(new TimeSpan(0, 0, 10));
+
+                sb3.Begin(currentpage);
+                sb4.Begin(nextpage);
+
+          
+
+                //currentpage.Visibility = System.Windows.Visibility.Collapsed;
                 nextpage.Visibility = System.Windows.Visibility.Visible;
+                //currentSub.Visibility = System.Windows.Visibility.Collapsed;
+                nextSub.Visibility = System.Windows.Visibility.Visible;
             }
         }
         private void SwitchNext()
@@ -70,10 +102,28 @@ namespace se306p2
             if (IsNextValid())
             {
                 Grid currentpage = pages.ElementAt(indicator);
+                Grid currentSub = subs.ElementAt(indicator);
                 indicator += 1;
                 Grid nextpage = pages.ElementAt(indicator);
-                currentpage.Visibility = System.Windows.Visibility.Collapsed;
+                Grid nextSub = subs.ElementAt(indicator);
+
+
+                // animations
+                Storyboard sb1 = (Storyboard)Resources["SlideOriginToRight"];
+                Storyboard sb2 = (Storyboard)Resources["SlideLeftToOrigin"];
+                sb1.Begin(currentSub);
+                sb2.Begin(nextSub);
+
+                Storyboard sb3 = (Storyboard)Resources["FadeOut"];
+                Storyboard sb4 = (Storyboard)Resources["FadeIn"];
+                sb3.Begin(currentpage);
+                sb4.Begin(nextpage);
+
+
+                //currentpage.Visibility = System.Windows.Visibility.Collapsed;
                 nextpage.Visibility = System.Windows.Visibility.Visible;
+                //currentSub.Visibility = System.Windows.Visibility.Collapsed;
+                nextSub.Visibility = System.Windows.Visibility.Visible;
             }
         }
         private bool IsPreviousValid()
