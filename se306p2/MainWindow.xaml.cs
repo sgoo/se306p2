421a1491c79126d2b949466029a70603592a4696
Scott Goodhew <scott.goodhew0@gmail.com>
2012-10-07 16:22:00 +1300

    map looking good

--- se306p2/MainWindow.xaml.cs
+++ se306p2/MainWindow.xaml.cs
@@ -72,20 +72,16 @@ namespace se306p2
             DataContext = this;
             LeftItems.Add(new DataItem("Home", true, new HomePage()));
             LeftItems.Add(new DataItem("Intro to ECE", true, new BaseInfo()));
-            LeftItems.Add(new DataItem("Base Info", true, new CourseSE()));
             LeftItems.Add(new DataItem("HOD's Welcome", true, new HODpage()));
-            LeftItems.Add(new DataItem("General", false));
-            LeftItems.Add(new DataItem("Location", true, new ContactPage()));
-            LeftItems.Add(new DataItem("blah", false));
             LeftItems.Add(new DataItem("Course Advisors", false));
-            LeftItems.Add(new DataItem("Contact/Location", false));
+			LeftItems.Add(new DataItem("Contact/Location", true, new ContactPage()));
 
             RightItems.Add(new DataItem("CSE Info", false,"#0b9246"));
             RightItems.Add(new DataItem("CSE Courses", false, "#0b9246"));
             RightItems.Add(new DataItem("EEE Info", false,"#1e9ad5"));
             RightItems.Add(new DataItem("EEE Courses", false,"#1e9ad5"));
             RightItems.Add(new DataItem("SE Info", false,"#f6a220"));
-            RightItems.Add(new DataItem("SE Courses", false,"#f6a220"));
+			RightItems.Add(new DataItem("SE Courses", true, new CourseSE(), "#f6a220"));
         }
 
 
