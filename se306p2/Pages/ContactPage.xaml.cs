5d495b91e00061547f2a05005146bb849c77c86d
Scott Goodhew <scott.goodhew0@gmail.com>
2012-10-14 15:10:05 +1300

    fixed map page

--- se306p2/Pages/ContactPage.xaml.cs
+++ se306p2/Pages/ContactPage.xaml.cs
@@ -11,19 +11,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Maps.MapControl.WPF;
 
 namespace se306p2.Pages {
 	/// <summary>
 	/// Interaction logic for ContactPage.xaml
 	/// </summary>
 	public partial class ContactPage : UserControl {
+		private Location previousFrameCenter;
+
 		public ContactPage() {
 			InitializeComponent();
+			bingMap.ViewChangeOnFrame += map_ViewChangeOnFrame;
 		}
 		private void UserControl_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e) {
 			if (IsVisible) {
 				MainWindow.window.BackgroundImage = "/Resources/themes/general.jpg";
 			}
 		}
+
+		private void map_ViewChangeOnFrame(object sender, MapEventArgs e) {
+			if (bingMap.ZoomLevel < 12 && previousFrameCenter != null) {
+				bingMap.SetView(previousFrameCenter, 12);
+
+				e.Handled = true; // Do we need it???
+			} else {
+				previousFrameCenter = bingMap.Center;
+			}
+
+			/*if (this.DoUpdate()) {
+				// Avoiding "jerking" effect and updating only for certain zoom/pan deltas
+				this.UpdateModelViewPort();
+			}*/
+		}
+
 	}
 }
